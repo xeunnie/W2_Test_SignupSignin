c74b9344435bcbf69db3d64ccb89af9da09fc7f6
Choi Seung-eun <138289674+xeunnie@users.noreply.github.com>
2025-02-03 17:17:20 +0900

    Update jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,8 @@
 module.exports = {
     testEnvironment: "jsdom",
     setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+    collectCoverage: true, // 커버리지 리포트 활성화
+    coverageDirectory: "coverage",
+    coverageReporters: ["lcov", "text-summary"], // Codecov가 인식할 수 있도록 lcov 포맷 추가
+    testEnvironment: "jsdom",
 };
