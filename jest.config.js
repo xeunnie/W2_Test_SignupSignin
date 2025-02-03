22ae71b3791dd7ee8a2f62a78ea720c13537e990
chloe_choi <plumxeun@gmail.com>
2025-02-03 17:56:49 +0900

    test

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,11 @@
-const nextJest = require('next/jest');
-
-const createJestConfig = nextJest({
-  dir: './',
-});
-
-module.exports = createJestConfig({
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom', // ✅ "node" → "jsdom"으로 변경
-  moduleDirectories: ['node_modules', '<rootDir>/'],
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'text-summary'],
-});
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
