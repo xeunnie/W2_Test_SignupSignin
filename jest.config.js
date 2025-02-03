439b01bb83c6a7a2e6e85aa0e0a0d688d4837091
Choi Seung-eun <138289674+xeunnie@users.noreply.github.com>
2025-02-03 17:38:30 +0900

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,18 @@
-import type { Config } from 'jest'
-import nextJest from 'next/jest.js'
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-    dir: './',
-})
+  dir: './',
+});
 
-const config: Config = {
-    coverageProvider: 'v8',
-    testEnvironment: 'jsdom',
-    moduleDirectories: ['node_modules', '<rootDir>/'],
-
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1',
-    },
-
-    setupFilesAfterEnv: ['./jest.setup.ts'],
-
-    // 추가된 커버리지 설정 (xeunnie-patch-9 브랜치 코드 통합)
-    collectCoverage: true,
-    coverageDirectory: "coverage",
-    coverageReporters: ["lcov", "text-summary"], // Codecov 포맷 추가
-}
-
-export default createJestConfig(config)
+module.exports = createJestConfig({
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: ['./jest.setup.js'], // TypeScript 설정이면 .ts 그대로 유지
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text-summary'],
+});
